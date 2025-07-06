431f0bef10dd845bfc0c690fa1cbaeea34d07a02
lien-nguyen <moonlierose@gmail.com>
2025-07-06 19:44:52 +0200

    Modified install-maven.sh

--- scripts/install-maven.sh
+++ scripts/install-maven.sh
@@ -18,24 +18,17 @@ mv apache-maven-3.9.10 maven
 # Remove the downloaded tar file to save space
 # rm apache-maven-3.9.10-bin.tar.gz
 
-# Add environment variables to user's .profile (preferred approach)
-echo "" >> ~/.profile
-echo "# Add environments for Java, Maven" >> ~/.profile
-echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> ~/.profile
-echo "export M2_HOME=/opt/maven" >> ~/.profile
-echo "export M2=/opt/maven/bin" >> ~/.profile
-echo "export PATH=\$PATH:\$HOME/bin:\$JAVA_HOME/bin:\$M2" >> ~/.profile
+# Add environment variables to .bashrc
+echo "" >> ~/.bashrc
+echo "# Add environments for Java, Maven" >> ~/.bashrc
+echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> ~/.bashrc
+echo "M2_HOME=/opt/maven" >> ~/.bashrc
+echo "M2=/opt/maven/bin" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "PATH=\$PATH:\$HOME/bin:\$JAVA_HOME/bin:\$M2" >> ~/.bashrc
 
-# Alternative: Add system-wide environment variables (uncomment if needed)
-# echo "" >> /etc/profile
-# echo "# Add environments for Java, Maven" >> /etc/profile
-# echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> /etc/profile
-# echo "export M2_HOME=/opt/maven" >> /etc/profile
-# echo "export M2=/opt/maven/bin" >> /etc/profile
-# echo "export PATH=\$PATH:\$HOME/bin:\$JAVA_HOME/bin:\$M2" >> /etc/profile
-
-# Reload the .profile to apply changes
-source ~/.profile
+# Reload the .bashrc to apply changes
+source ~/.bashrc
 
 # Verify installation
 echo "Maven installation completed!"
